2b4e8ffd08e52c3ace46ce17f19e78b780265818
Vilas-Gowda <vilasgowda65@gmail.com>
2021-11-22 21:51:16 +0530

    day 2
    ui part is getting some work

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-print('''Welcome to the Snake game!
-1.Start
-2.Exit''')
-run_game = int(input(""))
-while run_game >2 or run_game<1:
-    run_game = input("Invalid input choose btw 1 or 2")
+import ui
+import time
+import random
+
+run = ui.start_ui()
+
+
+
+
+
+
+
 
 
 
--- ui.py
+++ ui.py
@@ -1,6 +1,33 @@
-a = []
-for x1 in range(10):
-    for x2 in range(10):
-        a[x1][x2].append(' ')
+from os import system,name
 
-print(a)
+def start_ui():
+    print("Welcome to the Snake game!".center(100))
+    print("1.start".center(100))
+    print("2.exit ".center(100))
+    run_game = int(input(""))
+    while run_game >2 or run_game<1:
+        run_game = input("Invalid input choose btw 1 or 2")
+
+    if run_game == 2:
+        clear()
+        print("Thanks for playing!".center(100,'-'))
+        exit()
+    return run_game
+
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')   
+    else:
+        _ = system('clear')
+
+
+clear()
+a = [[' ' for i in range(100)] for j in range(20)]
+def output_ui():
+    for i in a:
+        for j in i:
+            print(j , end = '')
+        print('\n')
+
+
