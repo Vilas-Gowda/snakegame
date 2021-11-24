3a6c02794f253add1d3cf4b574e8f90bfe9be69b
Vilas-Gowda <vilasgowda65@gmail.com>
2021-11-24 22:09:43 +0530

    day 4

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
-import ui
-from ui import a 
+import functions
+from functions import a
+import random
 
-run_game = ui.start_ui()
+run_game = functions.start_ui()
 
-x = 4
+x = 7
 y = 25
 
 while run_game == 1:
     a[x][y] = '0'
-    ui.clear()
-    ui.output_ui()
+    functions.clear()
+    functions.output_ui()
     move = input("")
     if move.upper() == 'W':
         a[x][y] = ' '
         x = x-1
-        ui.wallencounter(x,y)
         a[x][y] = 'O'
     elif move.upper() == 'S':
         a[x][y] = ' '
         x = x+1
-        ui.wallencounter(x,y)
         a[x][y] = 'O'
     elif move.upper() == 'A':
         a[x][y] = ' '
         y = y-1
-        ui.wallencounter(x,y)
         a[x][y] = 'O'
     elif move.upper() == 'D':
         a[x][y] = ' '
         y = y+1
-        ui.wallencounter(x,y)
         a[x][y] = 'O'
+    
 
 
 
